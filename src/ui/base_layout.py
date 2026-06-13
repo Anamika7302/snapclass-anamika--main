ff078d2a5d13868d0c38dd3e035402d089e16d2d
Anamika7302 <tirkeyanamika98@gmail.com>
2026-06-13 11:10:37 +0530

    voice+face pipeline

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -52,12 +52,13 @@ def style_base_layout():
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
             @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
-                #MainMenu, footer , header{
-                visibility:hidden;
-                }
+               
 
                 .block-container{
                     padding-top:1.5cm !important;
+                }
+                 #MainMenu, footer , header{
+                visibility:hidden;
                 }
 
                 h1 {
