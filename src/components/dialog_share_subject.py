1d1505f1637847f63818234ed1533ccfb2f1692d
Anamika7302 <tirkeyanamika98@gmail.com>
2026-06-16 14:51:12 +0530

    fix filename

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -8,7 +8,7 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "snapclass--main-anamika.streamlit.app"
+    app_domain = "snapclass--main-anam.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
     
 
